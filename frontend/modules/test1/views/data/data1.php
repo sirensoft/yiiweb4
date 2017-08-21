25f735fdb2fa666045bc1c0519832fc2db62d003
tehnn <tehnnn@gmail.com>
2017-08-21 13:54:50 +0700

    utehn-init

--- frontend/modules/test1/views/data/data1.php
+++ frontend/modules/test1/views/data/data1.php
@@ -8,10 +8,53 @@ echo $data2;
 echo '<hr>';
 echo 'เริ่ม Array';
 echo '<br>';
-$array1 = ['a','b','c',10,'ก'];
+$array1 = ['a','b','c',10,'ก']; //element  //member
 echo $array1[2];  // c
 $array1[5]='xxxx';
 echo '<br>';
 echo '<pre>';
 print_r($array1); 
 echo '</pre>';
+$array2 = [
+    'name'=>'อุเทน',
+    'lname'=>'จาดยางโทน',
+    'age'=>37,
+    'office'=>[
+        'y2546'=>['สอ.A','สอ.B'],
+        'y2560'=>'สสจ.พล'
+    ]
+];
+echo $array2['lname'];
+echo '<hr>';
+echo $array2['office']['y2560'];
+
+echo $array2['office']['y2546'][1];
+
+
+echo '<hr>';
+$array3 = [];
+$array3[] = [
+    'name'=>'a',
+    'lname'=>'aaaa'
+];
+
+$array3[] = [
+    'name'=>'b',
+    'lname'=>'bbb'
+];
+
+
+echo '<pre>';
+print_r($array3);
+echo '</pre>';
+
+
+
+
+
+
+
+
+
+
+
