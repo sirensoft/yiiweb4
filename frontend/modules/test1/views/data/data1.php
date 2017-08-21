ea333d95aceac03d34759c9e6ba3503413a2ec88
tehnn <tehnnn@gmail.com>
2017-08-21 13:26:59 +0700

    utehn-init

--- frontend/modules/test1/views/data/data1.php
+++ frontend/modules/test1/views/data/data1.php
@@ -5,3 +5,11 @@ echo $data;
 echo '<hr>';
 $data2 = 100*6;
 echo $data2;
+echo '<hr>';
+echo 'เริ่ม Array';
+echo '<br>';
+$array1 = ['a','b','c',10,'ก'];
+echo $array1[2];  // c
+$array1[5]='xxxx';
+echo '<br>';
+print_r($array1); 
