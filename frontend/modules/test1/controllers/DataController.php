4088e383aa8cf04e1f8a26a68c3c44bb77e187fa
tehnn <tehnnn@gmail.com>
2017-08-21 13:17:32 +0700

    utehn-init

--- frontend/modules/test1/controllers/DataController.php
+++ frontend/modules/test1/controllers/DataController.php
@@ -1,15 +1,18 @@
 <?php
+
 namespace frontend\modules\test1\controllers;
+
 use yii\web\Controller;
 
-class DataController extends Controller{
-	public function actionData1(){
-		return $this->render('data1');
-	}
+class DataController extends Controller {
 
-	public function actionData2(){
-		return $this->render('data2');
-	}
+    public function actionData1() {
+        return $this->render('data1');
+    }
+
+    public function actionData2() {
+        return $this->render('data2');
+    }
 
 }
 
--- frontend/modules/test1/views/data/data1.php
+++ frontend/modules/test1/views/data/data1.php
@@ -1,1 +1,7 @@
-data1
+<?php
+
+$data = 'ข้อมูล-1';
+echo $data;
+echo '<hr>';
+$data2 = 100*6;
+echo $data2;
