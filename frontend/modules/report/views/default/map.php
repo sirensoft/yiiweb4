6acfd16511373ee7567e3e6455b79a9d3a57b75e
tehnn <tehnnn@gmail.com>
2017-08-24 10:28:19 +0700

    utehn-init

--- frontend/modules/report/views/default/map.php
+++ frontend/modules/report/views/default/map.php
@@ -9,9 +9,37 @@
         <style>
             body { margin:0; padding:0; }
             #map { position:absolute; top:0; bottom:0; width:100%; }
+            .info {
+                padding: 6px 8px;
+                font: 14px/16px Arial, Helvetica, sans-serif;
+                background: white;
+                background: rgba(255, 255, 255, 0.8);
+                box-shadow: 0 0 15px rgba(0, 0, 0, 0.2);
+                border-radius: 5px;
+            }
+
+            .info h4 {
+                margin: 0 0 5px;
+                color: #777;
+            }
+
+            .legend {
+                text-align: left;
+                line-height: 18px;
+                color: #555;
+            }
+
+            .legend i {
+                width: 18px;
+                height: 18px;
+                float: left;
+                margin-right: 8px;
+                opacity: 0.7;
+            }
         </style>
     </head>
     <body>
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
         <?php
 
         use yii\helpers\Url;
@@ -69,8 +97,7 @@ $route = Url::to(['/report/json/tambon']);
                 'ขอบเขตตำบล': tmbLayer.addTo(map)
             }).addTo(map);
 
-            var legend = L.control({'position': 'buttomright'});
-
+            var legend = L.control();
             legend.onAdd = function (map) {
                 var div = L.DomUtil.create('div', 'info legend');
                 var labels = ['<b>คำอธิบาย</b>'];
